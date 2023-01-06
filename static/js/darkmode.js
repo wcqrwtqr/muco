ec27beed4233b177ce68951951954c2153d3ae12
wcqrwtqr <mohalbatati@hotmail.com>
2023-01-06 19:47:51 +0300

    fixed the darkmode

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,19 +1,38 @@
-const button = document.querySelector('#dark-mode-button');
+const button2 = document.querySelector('#dark-mode-button-presist');
 const body = document.querySelector('body');
 const darkThemeCSS = document.querySelector('#dark-theme-css');
 const striped = document.querySelector('.table-striped');
+const filterform = document.querySelector('.card.card-body');
+const actionButton = document.querySelector('.btn.dropdown-toggle');
+const dropdowntheme = document.querySelector('.dropdown-menu');
 
+function toggleDarkTheme() {
+  body.classList.toggle('dark-theme');
+  filterform.classList.toggle('black-background');
+  striped.classList.toggle('table-striped');
+  // actionButton.classList.toggle('btn-dark');
+  // dropdowntheme.classList.toggle('dropdown-menu-dark');
+  localStorage.setItem('darkThemeEnabled',
+                       body.classList.contains('dark-theme'));
+}
 
-button.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
-    striped.classList.toggle('table-striped');
-    if (body.classList.contains('dark-theme')) {
-        document.documentElement.style.setProperty('--bg-color', '#333');
-        document.documentElement.style.setProperty('--text-color', '#fff');
-        document.documentElement.style.setProperty('--link-color', '#5cacee');
-    } else {
-        document.documentElement.style.setProperty('--bg-color', '#fff');
-        document.documentElement.style.setProperty('--text-color', '#333');
-        document.documentElement.style.setProperty('--link-color', '#0000ff');
-    }
-});
+function applySavedTheme() {
+  if (localStorage.getItem('darkThemeEnabled') === 'true') {
+    body.classList.add('dark-theme');
+    filterform.classList.add('black-background');
+    striped.classList.remove('table-striped');
+    // dropdowntheme.classList.add('dropdown-menu-dark');
+    // actionButton.classList.remove('btn-light');
+    // actionButton.classList.add('btn-dark');
+  } else {
+    body.classList.remove('dark-theme');
+    filterform.classList.remove('black-background');
+    striped.classList.add('table-striped');
+    // actionButton.classList.add('btn-light');
+    // actionButton.classList.remove('btn-dark');
+    // dropdowntheme.classList.remove('dropdown-menu-dark');
+  }
+}
+
+applySavedTheme();
+
